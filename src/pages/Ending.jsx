ed6295d7c6b26f5194cb41cb2da86db726fe74a2
Mathias Wagner <germannewsmaker@gmail.com>
2023-07-28 20:28:04 +0200

    Updated the Ending.jsx

--- src/pages/Ending.jsx
+++ src/pages/Ending.jsx
@@ -4,9 +4,11 @@ import HoverButton from "../components/HoverButton.jsx";
 import {t} from "i18next";
 import Navigation from "../components/Navigation.jsx";
 
-const Ending = ({guessed, max, reset, setPage, goBack}) => {
+const Ending = ({wrong, guessed, max, reset, setPage, goBack}) => {
 
     const [color, setColor] = useState("");
+
+    const name = (name) => name.toString().charAt(0).toUpperCase() + name.toString()?.slice(1);
 
     useEffect(() => {
         const prozent = guessed / max * 100;
@@ -27,6 +29,13 @@ const Ending = ({guessed, max, reset, setPage, goBack}) => {
                 <h1 style={{color}}>{Math.round(guessed / max * 100)}%</h1>
 
                 <h2>{guessed}/{max} {t("correct")}</h2>
+
+                {wrong.length > 0 && <div className="hmb wrong-hmb">
+                    {wrong.map((w) => <div className="wrong" key={w.country}>
+                        <img src={w.url} alt={w.country}/>
+                        <p>{name(w.country.split(",")[0])}</p>
+                    </div>)}
+                </div>}
             </div>
 
             <div className="row rw">
