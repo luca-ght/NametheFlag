864d0898b1ce2c143329e2ddeaff3f2641fdc678
luca <luca.gehlert2@gmail.com>
2023-07-28 02:33:39 +0200

    updated i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -2,12 +2,13 @@ import i18n from "i18next";
 import {initReactI18next} from "react-i18next";
 import LanguageDetector from 'i18next-browser-languagedetector';
 import HttpApi from 'i18next-http-backend';
+import {languages} from "./pages/Language.jsx";
 
 if (localStorage.getItem('language') === null)
     localStorage.setItem('language', navigator.language.split('-')[0]);
 
 i18n.use(initReactI18next).use(LanguageDetector).use(HttpApi).init({
-    supportedLngs: ['de', 'en', 'es', 'it', 'fr', 'pt', 'tr', 'gr', 'ru', 'cn', 'jp', 'kr'],
+    supportedLngs: Object.keys(languages),
     fallbackLng: 'de',
     backend: {
         loadPath: '/locales/{{lng}}.json'
