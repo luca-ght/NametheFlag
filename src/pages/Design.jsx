3b8c5955009539a14ad1da39154433c57899dc87
Luca <luca.gehlert2@gmail.com>
2024-05-28 02:58:21 +0200

    update

--- src/pages/Design.jsx
+++ src/pages/Design.jsx
@@ -3,10 +3,11 @@ import {t} from "i18next";
 import Navigation from "../components/Navigation.jsx";
 
 const colors = [
-    "#000000", "#3a2516", "#6D0C0C", "#042f03", "#350751", "#120F9A",
+    "#000000", "#3a2516", "#6D0C0C", "#042f03", "#350751", "#0e0c69",
     "#2f2f2f", "#73482F", "#D87316", "#136e24", "#6900ad", "#0250bb",
     "#545454", "#ad6e4a", "#FFD700", "#1DE619", "#F434CA", "#19AFE6",
     "#ffffff", "#ffb68a", "#b46363", "#0d724c", "#c798ec", "#6368bd",
+
 ];
 
 const Design = ({goBack, setPrimaryColor}) => {
